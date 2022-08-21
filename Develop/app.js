a0e98b330a93a04dfcda4fd814278d983dca24a8
Blackwell101 <103456893+Blackwell101@users.noreply.github.com>
2022-08-21 15:23:09 -0400

    creating javascript for project

--- Develop/app.js
+++ Develop/app.js
@@ -1,10 +1,55 @@
-$(document).ready (function() {
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+var currentDayEl = $('currentDay')
+var hourBlocksArea = $('hourBlocks')
 
-    $(".saveBtn").normalize("click", function (){
-        console.log(this);
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, text);
-    });
-});
+var currentDay = moment().format('dddd, mmmm, Do, yyyy');
+var currentTime = moment().format('H');
+var selectedDay = moment().format('MM/DD/YY')
+var storedSchedules = [];
+
+currentDayEl.text(currentDay);
+
+function getStoredSchedule() {
+    storedSchedules = JSON.parse(localStorage.getItem("allStoredSchedules"));
+    if (!storedSchedules) {
+        storedSchedules = [];
+        createdBlankSchedule();
+    }
+}
+
+function createdBlankSchedule() {
+    storedSchedules.push({day: selectedDay, hour:8, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:9, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:10, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:11, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:12, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:13, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:14, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:15, detail: "" })
+    storedSchedules.push({day: selectedDay, hour:16, detail: "" })
+    localStorage.setItem("allStoredSchedules", JSON.stringify(storedSchedules))
+}
+
+function createHourBlocks() {
+    var todaySchedules = storedSchedules.filter(schedules => {
+        return schedules.day == selectedDay});
+        if(!todaySchedules) {
+            createdBlankSchedule();
+            return createdHourBlocks();
+        };
+        for (var i = 8; i < 17; i++) {
+            var thisHourDetails = todaySchedules.find(hourDetails => hourDetails.hour === i)
+            var state = getHourState(i)
+            var hourBlockEl = $('<lable>', {
+                class: "col-2 hour",
+            });
+            var hourBlockInputEl = $('<textarea>',{
+                class: "col " + state,
+                placeholder: "Nothing Scheduled"
+            });
+            var hourBlockBtnEl = $('<button>', {
+                class: "col-1 saveBtn"
+            });
+            
+        }
+    
+}
